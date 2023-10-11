4ff88ae2f530899ba37d81c839fbc0c739594954
artur litnarovych <doubledoge246@gmail.com>
2023-10-11 18:52:48 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,13 +8,13 @@ import store from "./redux/store";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/AddEditTour.js
+++ src/pages/AddEditTour.js
@@ -7,7 +7,7 @@ import {
   MDBBtn,
   MDBSpinner,
 } from "mdb-react-ui-kit";
-import ChipInput from "material-ui-chip-input";
+import { MuiChipsInput } from 'mui-chips-input';
 import FileBase from "react-file-base64";
 import { toast } from "react-toastify";
 import { useNavigate, useParams } from "react-router-dom";
@@ -113,7 +113,7 @@ const AddEditTour = () => {
               />
             </div>
             <div className="col-md-12">
-              <ChipInput
+              <MuiChipsInput
                 name="tags"
                 variant="outlined"
                 placeholder="Countries"
